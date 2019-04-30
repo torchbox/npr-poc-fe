9035595e7088bf45e6601bc7e6584eff8b668344
thisoldbear <danielwilliams.design@gmail.com>
2019-04-30 16:08:54 +0100

    Style footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,29 @@
 import React from "react";
 
-import { StyledFooter } from "./styled";
+import {
+  StyledFooter,
+  StyledFooterInner,
+  StyledFooterAbout,
+  StyledFooterHeading,
+  StyledFooterText
+} from "./styled";
 
-const Footer = ({ visibility }) => <StyledFooter visiblity={visibility} />;
+const Footer = ({ visibility }) => (
+  <StyledFooter visiblity={visibility}>
+    <StyledFooterInner>
+      <StyledFooterAbout>
+        <StyledFooterHeading>Torchbox Radio</StyledFooterHeading>
+        <StyledFooterText>
+          Listener-supported WNYC is the home for independent journalism and
+          courageous conversation on air and online. Broadcasting live from New
+          York City on 93.9 FM and AM 820 and available online and on the go.
+        </StyledFooterText>
+        <StyledFooterText>
+          © Torchbox Radio 2019
+        </StyledFooterText>
+      </StyledFooterAbout>
+    </StyledFooterInner>
+  </StyledFooter>
+);
 
 export default Footer;
--- src/components/footer/styled.js
+++ src/components/footer/styled.js
@@ -5,4 +5,27 @@ const StyledFooter = styled.footer`
   height: 400px;
 `;
 
-export { StyledFooter };
+const StyledFooterInner = styled.div`
+  max-width: ${props => props.theme.grid.maxWidth};
+  margin: 0 auto;
+  padding: 80px ${props => props.theme.grid.outerGutter};
+`;
+
+const StyledFooterAbout = styled.div`
+  max-width: 480px;
+`;
+
+const StyledFooterHeading = styled.h3`
+  font-size: 16px;
+  font-weight: ${props => props.theme.fonts.bold};
+  color: ${props => props.theme.colors.blue};
+  text-transform: uppercase;
+`;
+
+const StyledFooterText = styled.p`
+  font-size: 16px;
+  color: ${props => props.theme.colors.grey};
+  line-height: 22px;
+`;
+
+export { StyledFooter, StyledFooterInner, StyledFooterAbout, StyledFooterHeading, StyledFooterText };
